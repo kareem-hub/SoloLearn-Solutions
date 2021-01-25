45b873a874033b0395937df7d1797b831f9be60a
kareem-hub <62149929+kareem-hub@users.noreply.github.com>
2021-01-25 23:29:08 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+def fibonacci(n):
+    lst=[0,1]
+    for a in range(2,n):
+        lst.append(lst[a-1]+lst[a-2])
+    for e in lst:
+        print(e)
+        
+fibonacci(num)
