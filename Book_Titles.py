7d7a4b380847b8049b3df397e4969844b32cd2b1
kareem-hub <62149929+kareem-hub@users.noreply.github.com>
2021-01-25 23:37:09 +0200

    Add files via upload

---
+++ Book_Titles.py
@@ -0,0 +1,9 @@
+file = open("/usercode/files/books.txt", "r")
+
+for line in file.readlines():
+    if "\n" in line:
+        print(line[0]+str(len(line)-1))
+    else:
+        print(line[0]+str(len(line)))
+
+file.close()
---
+++ longest_word.py
@@ -0,0 +1,8 @@
+#take & store words in a list
+txt = input().split()
+#get length of longest word in the txt list
+maxwrd=max(len(i) for i in txt)
+#loop over txt, if the word length == maxwrd print the word
+for word in txt:
+    if len(word)==maxwrd:
+        print(word)
